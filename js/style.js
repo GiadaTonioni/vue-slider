aa483aed264a33a3182ff2771e0afcf2423b715c
GiadaTonioni <giada.tonioni@gmail.com>
2023-06-08 17:24:46 +0200

    aggiunta frecce non posizionate

--- js/style.js
+++ js/style.js
@@ -10,17 +10,28 @@ createApp({
                     './img/03.webp',
                     './img/04.webp',
                     './img/05.webp',
-                ],
-                didascalie: [
-                    'Spiderman',
-                    'didascalia',
-                    'didascalia',
-                    'didascalia',
-                    'didascalia'
                 ]
             },
             activeImage: 0
         }
+    },
+    methods: {
+        changeImage(index){
+            this.activeImage = index
+        },
+        nextImage(){
+            this.activeImage ++;
+            if(this.activeImage > this.serie.images.length - 1){
+                this.activeImage = 0
+            }
+        },
+        previousImage(){
+            this.activeImage --;
+            if(this.activeImage < 0){
+                this.activeImage = this.serie.images.length - 1;
+            }
     }
+}
+    
 }).mount('#app')
 
